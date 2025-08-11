4fd96a161d0e524a29d279b8d8931bfa02f70675
Ahmed <a.jassim2959@gmail.com>
2025-08-11 10:05:14 +0300

    added the habit log schema

--- models/Habit.js
+++ models/Habit.js
@@ -1,30 +1,37 @@
 const mongoose = require("mongoose");
 
+const habitLog = new mongoose.Schema({}, { timestamps: true });
 
-const habitSchema = new mongoose.Schema({
-  habitOwner: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
+const habitSchema = new mongoose.Schema(
+  {
+    habitOwner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+
+    habitName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    habitDescription: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    habitFrequency: {
+      type: String,
+      required: true,
+      enum: ["Daily", "Weekly", "Monthly"],
+    },
+    habitLog: [habitLog],
   },
+  { timestamps: true }
+);
 
-  habitName: {
-    type: String,
-    required: true,
-    trim: true,
-  },
+const Habit = mongoose.model("Habit", habitSchema);
 
-  habitDescription: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-
-  habitFrequency: {
-    type: String,
-    required: true,
-    enum: ["Daily", "Weekly", "Monthly"],
-  },
-  habitLog: [habitLog],
-});
-
+module.exports = Habit;
